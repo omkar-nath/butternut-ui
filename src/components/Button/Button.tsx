4c20f7d6d469b96a237cd8008aa05360f746098f
omkar-nath <omkarnath8039@gmail.com>
2022-08-14 04:55:17 +0100

    fix: build folder generation and export

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import "./Button.css";
+import styled from 'styled-components';
+
+const ButtonComp = styled('button')`
+    font-size:30px
+`
 
 export interface ButtonProps{
     label:string;
 }
 
 const Button = (props:ButtonProps)=>{
-    return (<button>{props.label}</button>);
+    return (<ButtonComp>{props.label}</ButtonComp>);
 }
 
 export default Button;
